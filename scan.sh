351ab2cbb52eb5667c263c9760dc5261390f077a
HerrBrauhaus1337 <ycwiegman@pm.me>
2026-03-03 10:50:14 +0100

    Update scan.sh

--- scan.sh
+++ scan.sh
@@ -1,3 +1,9 @@
+#Check size of the file in the archive
+SIZE=$(7z l -slt ARCHIVE_NAME.7z | awk -F' = ' '
+  $1=="Path" && $2=="ARCHIVE_CONTENTS.txt" {found=1}
+  found && $1=="Size" {print $2; exit}
+')
+
 #Change the filenames in all caps to your desired value
 
 7z x -so ARCHIVE_NAME.7z ARCHIVE_CONTENTS.txt \
