43be89f8405e777725b11904d259bd2dfcd4b3f8
HerrBrauhaus1337 <ycwiegman@pm.me>
2026-03-03 13:43:43 +0100

    Delete Clean_key_file.sh

--- Clean_key_file.sh
+++
@@ -1,6 +0,0 @@
-clean_key=$(mktemp)
-#  - Remove Windows CR (\r)
-#  - Trim leading/trailing spaces/tabs
-#  - Delete completely empty lines
-sed 's/\r$//; s/^[[:space:]]*//; s/[[:space:]]*$//' Test.txt |
-grep -v '^$' >"$clean_key"
